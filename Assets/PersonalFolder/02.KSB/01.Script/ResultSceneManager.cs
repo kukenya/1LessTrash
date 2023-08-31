8ad68e5942b81789b5c317f5848420341cb89cd3
JoanneKael <JoanneKael@users.noreply.github.com>
2023-08-31 22:30:19 +0900

    4

--- Assets/PersonalFolder/02.KSB/01.Script/ResultSceneManager.cs
+++ Assets/PersonalFolder/02.KSB/01.Script/ResultSceneManager.cs
@@ -6,16 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class ResultSceneManager : MonoBehaviour
 {
+    // 오답노트 팝업
+    public GameObject note;
 
+    // 오답노트 내용
+    public GameObject[] notes;
 
     void Start()
     {
-        
-    }
+        // 팝업창 비활성화
+        note.SetActive(false);
 
-    void Update()
-    {
-        
+        // 오답노트 내용 비활성화
+        for (int i = 0; i < notes.Length; i++)
+        {
+            notes[i].SetActive(false);
+        }
     }
 
     // ReTry
@@ -31,6 +37,37 @@ public class ResultSceneManager : MonoBehaviour
         // 로비씬으로 -> 로비씬
         SceneManager.LoadScene("02.LobbyScene");
     }
+
+    #region 오답노트
+    public void OnClickNote()
+    {
+        // 오답인 문제의 번호를 받아온다. <- 서버에서
+
+
+        // 오답노트 팝업이 뜬다.
+        note.SetActive(true);
+
+        // 오답 문제의 UI만 전부 활성화한다.
+
+    }
+
+    public void OnClickPrev()
+    {
+        // 이전 내용
+        // currCount++;
+    }
+
+    public void OnClickNext()
+    {
+        // 다음 내용
+
+    }
+
+    public void OnClickClose()
+    {
+        note.SetActive(false);
+    }
+    #endregion
 
     // ExitGame
     public void OnClickExitGame()
